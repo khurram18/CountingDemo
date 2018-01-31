8cf89ad939883575f7d076e79e45859c3cb3cb3c
Khurram Shehzad <khurram.pak522@gmail.com>
2018-01-31 14:12:53 +0500

    Initial project

---
+++ app/src/main/java/com/example/countingdemo/CountingHelper.java
@@ -0,0 +1,60 @@
+package com.example.countingdemo;
+
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.TextView;
+
+/**
+ * Created by Khurram Shehzad on 31 January 2018
+ */
+
+public class CountingHelper implements Runnable {
+
+private final long mDuration;
+private final int mStartingValue;
+private final int mFinalValue;
+
+private final TextView mTextView;
+
+private final int mValueRange;
+
+private long mProgressTime = 0;
+private long mLastUpdateTime = 0;
+
+
+private Interpolator mInterpolator = new AccelerateInterpolator();
+
+public CountingHelper(int startingValue, int finalValue, int duration, TextView textView) {
+    mDuration = duration;
+    mStartingValue = startingValue;
+    mFinalValue = finalValue;
+    mTextView = textView;
+
+    mValueRange = mFinalValue - mStartingValue;
+
+    mLastUpdateTime = System.currentTimeMillis();
+}
+
+@Override
+public void run() {
+    long now = System.currentTimeMillis();
+    mProgressTime += (now - mLastUpdateTime);
+    boolean shouldSchedule = true;
+    if (mProgressTime >= mDuration) {
+        mProgressTime = mDuration;
+        shouldSchedule = false;
+    }
+    mTextView.setText(String.valueOf(getCurrentValue()));
+    if (shouldSchedule) {
+        mTextView.postOnAnimation(this);
+    }
+}
+
+private int getCurrentValue() {
+    if (mProgressTime >= mDuration) {
+        return mFinalValue;
+    }
+    return (int) (mStartingValue + (mInterpolator.getInterpolation(
+            (float) mProgressTime / (float) mDuration) * mValueRange));
+}
+}
---
+++ app/src/main/java/com/example/countingdemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.countingdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mTextView = findViewById(R.id.text_view);
+}
+
+@Override
+protected void onResume() {
+    super.onResume();
+    setValue(1500);
+}
+private void setValue(final int value) {
+    mTextView.post(new Runnable() {
+        @Override
+        public void run() {
+            mTextView.postOnAnimation(
+                    new CountingHelper(0, value, 5000, mTextView));
+        }
+    });
+}
+private TextView mTextView;
+}
